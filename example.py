a9f587b377a29c1240b8de0bf65345278d562bf3
doubletigerzju <475823032@qq.com>
2026-03-08 21:36:10 +0800

    Update example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from RecTAD import TADCaller
 import numpy as np
 
 ##读取原始数据并转换为密集矩阵形式
-path_input = "C:/Users/Lenovo/Desktop/testdata.txt"
+path_input = "Path to testdata/testdata.txt"
 graph_edge = np.loadtxt(path_input)
 chr='chr3'
 resolution = 50000
@@ -18,5 +18,6 @@ matrix_recon = reconstructor.getRecon()
 #调用TADCaller进行TAD边界检测
 caller = TADCaller(min_window=3, max_window=30)
 boundaries, TADs = caller.fit(matrix_recon)
+
 
 print("TAD boundaries:", boundaries)
